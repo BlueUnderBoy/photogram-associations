90eafacc0ec873730e871aa7bc118af0fbc8e539
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-11-12 21:23:20 +0000

    fichier de like a met a jour

--- app/models/like.rb
+++ app/models/like.rb
@@ -17,14 +17,8 @@ class Like < ApplicationRecord
   })
   belongs_to(:fan, class_name:"User", foreign_key:"fan_id")
   belongs_to(:liked_photo, class_name:"Photo", foreign_key:"photo_id")
-  belongs_to(:fan_like,
-    class_name: "User",
-    foreign_key: "fan_id"
-  )
-  has_many(:fan_likes,
-    through: :photo_likes,
-    source: :fan_like
-  )
+  belongs_to(:fan_like, class_name: "User", foreign_key: "fan_id")
+  has_many(:fan_likes, through: :photo_likes, source: :fan_like)
 
   # Association accessor methods to define:
   
@@ -34,23 +28,23 @@ class Like < ApplicationRecord
 
   # Like#photo: returns a row from the photo table associated to this like by the photo_id column
 
-  def fan
-    my_fan_id = self.fan_id
+  #def fan
+  #  my_fan_id = self.fan_id
+  #
+  #  matching_users = User.where({ :id => my_fan_id })
+  #
+  #  the_user = matching_users.at(0)
+  #
+  #  return the_user
+  #end
 
-    matching_users = User.where({ :id => my_fan_id })
-
-    the_user = matching_users.at(0)
-
-    return the_user
-  end
-
-  def photo
-    my_photo_id = self.photo_id
-
-    matching_photos = Photo.where({ :id => my_photo_id })
-
-    the_photo = matching_photos.at(0)
-
-    return the_photo
-  end
+  #def photo
+  #  my_photo_id = self.photo_id
+  #
+  #  matching_photos = Photo.where({ :id => my_photo_id })
+  #
+  #  the_photo = matching_photos.at(0)
+  #
+  #  return the_photo
+  #end
 end
